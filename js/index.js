c1cce1597ef29ffe99581649f56e6054e63deead
Sheila <rugut.cheruto@student.moringaschool.com>
2022-09-07 16:49:50 +0300

    creating a breed list

--- js/index.js
+++ js/index.js
@@ -3,9 +3,21 @@ async function init(){
    //await states js does not run any other function until the promise is resolved
    const response= await fetch("https://dog.ceo/api/breeds/list/all");
    const data =await response.json()
-   console.log(data.message)
+   createBreedList(data.message)
     }
     catch (error){
 console.log ("Problem accessing the breedlist")
 }
  }
+ init()  
+function createBreedList(breedList){
+document.getElementById("breed").innerHTML=`
+<select>
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList).map(function(breed){
+         return `<option>${breed}</option>`
+        }).join('')}
+
+      </select>
+`
+}
