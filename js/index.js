56371c10341c0b31ef8ce0061a6c56a6b3287f40
Sheila <rugut.cheruto@student.moringaschool.com>
2022-09-07 17:20:45 +0300

    completed js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let timer
 let removeFirstImageDelay
+//document.addEventListener("DOMContentLoaded", (event) => {function init()});
 async function init(){
     try {
    //await states js does not run any other function until the promise is resolved
@@ -11,56 +12,58 @@ async function init(){
 console.log ("Problem accessing the breedlist")
 }
  }
- init()  
- function createBreedList(breedList){
-    document.getElementById("breed").innerHTML=`
-    <select onchange="displayByBreed(this.value)">
-            <option>Choose a dog breed</option>
-            ${Object.keys(breedList).map(function(breed){
-             return `<option>${breed}</option>`
-            }).join('')}
-    
-          </select>
-    `
-    }
+init()  
+function createBreedList(breedList){
+document.getElementById("breed").innerHTML=`
+<select onchange="displayByBreed(this.value)">
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList).map(function(breed){
+         return `<option>${breed}</option>`
+        }).join('')}
+
+      </select>
+`
+}
+
 async function displayByBreed(breed){
-        if (breed !=="Choose a dog breed"){
-        const response=await fetch(`https://dog.ceo/api/breed/${breed}/images`)
-        const data=await response.json()
-        displaySlides(data.message)
- }
+if (breed !=="Choose a dog breed"){
+const response=await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+const data=await response.json()
+displaySlides(data.message)
+}
 }
 function displaySlides(images){
 let currentPosition=0
 clearInterval(timer)
 clearTimeout(removeFirstImageDelay)
+
 if (images.length > 1){
-    document.getElementById("slideshow").innerHTML=`
+  document.getElementById("slideshow").innerHTML=`
+<div class="slide" style="background-image: url('${images[0]}')"></div>
+<div class="slide" style="background-image: url('${images[1]}')"></div>
+`
+currentPosition +=2
+if (images.length == 2)currentPosition=0
+timer = setInterval(nextSlide, 3000)
+}
+else {
+  document.getElementById("slideshow").innerHTML=`
   <div class="slide" style="background-image: url('${images[0]}')"></div>
-  <div class="slide" style="background-image: url('${images[1]}')"></div>
+  <div class="slide"></div>
   `
-  currentPosition +=2
-  if (images.length == 2)currentPosition=0
-  timer = setInterval(nextSlide, 3000)
-  }
-  else {
-    document.getElementById("slideshow").innerHTML=`
-    <div class="slide" style="background-image: url('${images[0]}')"></div>
-    <div class="slide"></div>
-    `
-  }
-  function nextSlide(){
-    document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
-  removeFirstImageDelay=setTimeout(function(){
-    document.querySelector(".slide").remove()
-  }, 1000)
-  
-  if (currentPosition +1 >=images.length){
-     currentPosition=0
-  }
-  else{
-    currentPosition++
-  }
-  }
-  
+}
+
+function nextSlide(){
+  document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+removeFirstImageDelay=setTimeout(function(){
+  document.querySelector(".slide").remove()
+}, 1000)
+
+if (currentPosition +1 >=images.length){
+   currentPosition=0
+}
+else{
+  currentPosition++
+}
+}
 }
